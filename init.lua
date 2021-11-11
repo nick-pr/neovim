7fcbc344291382546063a762e0cfb180f78e2ae0
Nicholas Raffaele <nick@macbookair.home>
2021-11-11 17:49:31 -0500

    changed variable names once again

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local core_ok, core = pcall(require,'core')
 
 if not core_ok then
-	local core_error = core
-	print('Something went wrong in loading core.\n'..core_error)
+    local core_error = core
+    print('Something went wrong in loading core.\n'..core_error)
 end
 
 local plugins_ok, plugins = pcall(require,'plugins')
 
 if not plugins_ok then
-	local plugins_error = plugins
-	print('Something went wrong is loading plugins.\n'..plugins_error)
+    local plugins_error = plugins
+    print('Something went wrong is loading plugins.\n'..plugins_error)
 end
