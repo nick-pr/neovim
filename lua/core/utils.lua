546f8d5e5dd965d6cbe488504b72181ed3e97e35
Nicholas Raffaele <nick@macbookair.home>
2021-11-12 18:42:05 -0500

    added map funtion to utils

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -2,7 +2,6 @@ local M = {}
 local map_key = vim.api.nvim_set_keymap
 
 M.map = function(mode,key,command,opts)
-    print('inside map function')
     opts = opts or {}
     if type(mode) == 'string' then mode = {mode} end 
     for _,mode in ipairs(mode) do map_key(mode,key,command,opts)end
