7bdb7bb2656ca6d717791823438c8a0d2503848a
Nick Raffaele <Raffaele.nickp@gmail.com>
2021-12-19 21:34:47 -0500

    removed bracey

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,93 +8,51 @@ end
 packer.startup(function(use)
 
     use 'wbthomason/packer.nvim'
-
     use {
         'nvim-treesitter/nvim-treesitter',
         event = 'BufRead',
         config = function() require('plugins.configs.treesitter') end
     }
 
-    -- use {
-    --     'rmehri01/onenord.nvim',
-    --     disable = true,
-    -- }
- 
-    -- use {
-    --     'navarasu/onedark.nvim',
-    --     config = function() require('colors.themes.onedark') end,
-    -- }
-
---    use {
---         'ellisonleao/gruvbox.nvim',
---         disable = false,
---         requires = {"rktjmp/lush.nvim"},
---         config = function() require('colors.themes.gruvbox') end,
--- 
---     }
-
-    -- use {
-    --     'olimorris/onedarkpro.nvim',
-    --     disable = false,
-    --     config = function() require('colors.themes.onedarkpro') end,
-    -- }
--- 
     use {
         'folke/tokyonight.nvim',
-        disable = false,
         config = function() require('colors.themes.tokyonight') end,
     }
-    -- use {
-    --     'bluz71/vim-nightfly-guicolors',
-    --     config = function() require('colors.themes.nightfly') end
-    -- }
-
     use {
         'jiangmiao/auto-pairs',
         event = 'bufEnter'
     }
-
     use {
         'lewis6991/gitsigns.nvim',
         requires = {'nvim-lua/plenary.nvim'},
         config= function() require('plugins.configs.gitsigns') end,
         event = 'bufEnter'
     }
-
     use {
         'nvim-lualine/lualine.nvim',
         requires = {'kyazdani42/nvim-web-devicons', opt = true},
         config = function() require('plugins.configs.lualine') end,
     }
-
     use {
         'blackCauldron7/surround.nvim',
         event = 'bufEnter',
         config = function() require('plugins.configs.surround-nvim')end,
     }
-
     use {
         'terrortylor/nvim-comment',
         event = 'bufEnter',
         config = function() require('plugins.configs.nvim-comment') end
     }
-
     use {
         'kyazdani42/nvim-tree.lua',
         requires = {'kyazdani42/nvim-web-devicons', opt = true},
         cmd = {'NvimTreeToggle','NvimTreeFocus'},
         config = function() require('plugins.configs.nvim-tree') end
     }
-
     use {
         'mhartington/formatter.nvim',
         config = function() require('plugins.configs.formatter') end,
         cmd = {'Format','FormatWrite'}
-    }
-
-    use {
-        'turbio/bracey.vim',
-        cmd = 'Bracey'
     }
     use {
         'neovim/nvim-lspconfig',
