beb41a6d266409de97251f8d28ca033f024f3546
Nick Raffaele <Raffaele.nickp@gmail.com>
2021-12-19 21:14:19 -0500

    better terminal configs

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -35,3 +35,15 @@ map({'n','v'},'<leader>bf','<cmd>Format<CR>',{noremap = true, silent = true})
 
 -- LSP
 map({'n'},'gd','<cmd>lua vim.lsp.buf.definition()<CR>',{noremap = true, silent = true})
+
+-- Terminal
+map ('t','<ESC>','<C-\\><C-n>')
+map ('t','<C-w>j','<C-\\><C-n><C-w>j')
+map ('t','<C-w>k','<C-\\><C-n><C-w>k')
+map ('t','<C-w>h','<C-\\><C-n><C-w>h')
+map ('t','<C-w>l','<C-\\><C-n><C-w>l')
+
+map ('t','<C-w><Down>','<C-\\><C-n><C-w><Down>')
+map ('t','<C-w><Up>','<C-\\><C-n><C-w><Up>')
+map ('t','<C-w><Left>','<C-\\><C-n><C-w><Left>')
+map ('t','<C-w><Right>','<C-\\><C-n><C-w><Right>')
