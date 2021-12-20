567337d92ae125da594ed6def58d0ac5ccb2e79b
Nick Raffaele <Raffaele.nickp@gmail.com>
2021-12-19 21:13:46 -0500

    started customization of lualine

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -1,18 +1,49 @@
+local colors = require('colors.themes.tokyonight')
+local opts = {
+    padding = 1,
+}
+local componets = {
+    branch = {
+        'branch',
+        color = {
+            fg=colors.fg,
+            bg=colors.bg,
+        },
+        padding=opts.padding
+    },
+    diff = {
+        'diff',
+        color = {
+            bg=colors.bg,
+        },
+        symbols = {
+            added = ' ',
+            modified = ' ',
+            removed = ' ',
+        }
+    },
+    filename = {
+        'filename',
+        color = {
+            bg = colors.none,
+            fg = colors.fg,
+        }
+    }
+}
 require'lualine'.setup {
   options = {
     icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    theme = 'tokyonight',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
     disabled_filetypes = {},
-    always_divide_middle = true,
+    always_divide_middle = false,
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff',
-                  {'diagnostics', sources={'nvim_diagnostic', 'coc'}}},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_b = {componets.branch,componets.diff},
+    lualine_c = {componets.filename},
+    lualine_x = {{'filetype'}},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
@@ -20,7 +51,7 @@ require'lualine'.setup {
     lualine_a = {},
     lualine_b = {},
     lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_x = {},
     lualine_y = {},
     lualine_z = {}
   },
