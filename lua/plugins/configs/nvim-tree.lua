211efb6319106793239c348415fc9840b7384435
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-07-24 10:18:21 -0700

    [Nvim-tree] Indent markers, disabled folder arrows

--- lua/plugins/configs/nvim-tree.lua
+++ lua/plugins/configs/nvim-tree.lua
@@ -3,6 +3,22 @@ local map = vim.keymap.set
 -- Setup
 require("nvim-tree").setup {
 	update_cwd = true,
+	renderer = {
+		indent_markers = {
+			enable = true,
+			icons = {
+				corner = "└",
+				edge = "│",
+				item = "│",
+				none = " ",
+			},
+		},
+        icons = {
+            show = {
+                folder_arrow = false
+            }
+        }
+	},
 }
 
 -- Mappings
