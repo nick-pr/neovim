546ad232c0ce4cc9cbbf894bf50bea8e480b98c1
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-07-25 22:09:30 -0700

    [toggleterm] removed roundedness

--- lua/plugins/configs/toggleterm.lua
+++ lua/plugins/configs/toggleterm.lua
@@ -2,7 +2,7 @@ require("toggleterm").setup {
 	open_mapping = [[<c-\>]],
     direction = "float",
     float_opts = {
-        border = "curved"
+        border = "single"
     },
     shade_terminals = false
 }
