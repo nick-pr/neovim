efa6182eaa346cb999408d53dab774ee4182f10f
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-09-13 17:35:51 -0400

    fix(nvim-tree): fixes nvim-web-devicons error
    This error was caused my nvim-web-devicons being flagged as optional
    while nvim-tree was not optional.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -53,7 +53,7 @@ use {
 -- Nvim-tree, for a file tree within neovim.
 use {
 	"kyazdani42/nvim-tree.lua",
-	requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	requires = { "kyazdani42/nvim-web-devicons" },
 	config = useConfig "nvim-tree",
 }
 
