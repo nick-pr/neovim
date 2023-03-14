568963aab213e93a0bbecb50625402c638769be9
Nicholas Raffaele <nick@MacbookAir.local>
2023-03-14 09:38:23 -0400

    feat(gitsigns): add plugin GitSigns

---
+++ lua/plugins/git-signs.lua
@@ -0,0 +1,14 @@
+local M = { "lewis6991/gitsigns.nvim" }
+
+M.opts = {}
+
+M.config = function(lazy_plugin, opts)
+	local gitsigns = require "gitsigns"
+	local map = vim.keymap.set
+
+	gitsigns.setup(opts)
+
+	local map_opts = { noremap = true }
+	map({ "n" }, "gb", "<cmd>Gitsigns blame_line<CR>", map_opts)
+end
+return M
