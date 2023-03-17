d3b325824672efcf3c672807111a8681ec381036
Nick Raffaele <nick.raffaelep@uber.com>
2023-03-17 18:59:37 -0400

    feat(feline): Nvim tree winbar

--- lua/plugins/feline/init.lua
+++ lua/plugins/feline/init.lua
@@ -1,6 +1,8 @@
-local M = { "feline-nvim/feline.nvim" }
 local components = require "plugins/feline/components"
 local p = require "theme.palette"
+local utils = require "core.utils"
+
+local M = { "feline-nvim/feline.nvim" }
 
 Bar = {}
 MetaBar = {}
@@ -26,24 +28,65 @@ local winbar_components = {
 	inactive = Bar:new(),
 }
 
+local file_tree_components = {
+	active = Bar:new(),
+	inactive = Bar:new(),
+}
+
 winbar_components.active:append(1, components.file_info)
 winbar_components.active:append(1, { provider = "", hl = { bg = "none" } })
 
 winbar_components.inactive:append(1, components.file_info)
 winbar_components.inactive:append(1, { provider = "", hl = { bg = "none" } })
 
+----File Tree Bar -------
+file_tree_components.active:append(1, {
+	provider = utils.icon_from_hex "fb44" .. " ",
+	hl = { bg = "blue_500" },
+	left_sep = { str = " ", hl = { fg = "bg", bg = "blue_500" } },
+	right_sep = { str = "right_rounded", hl = { fg = "blue_500", bg = "bg" } },
+})
+file_tree_components.active:append(1, {
+	provider = {
+		name = "cwd_parent_dir",
+		update = { "DirChanged" },
+	},
+	hl = { style = "bold" },
+	-- icon = { str = utils.icon_from_hex "fb44" .. " ", hl = { fg = "orange_300" } },
+	left_sep = { str = " ", hl = { fg = "bg", bg = "bg" } },
+})
+file_tree_components.active:append(3, {
+	provider = " ",
+	hl = { bg = "bg" },
+	right_sep = { str = " ", hl = { fg = "bg", bg = "bg" } },
+})
+
 M.opts = {
 	winbar = {
 		components = winbar_components,
+		conditional_components = {
+			{
+				condition = function()
+					return vim.api.nvim_buf_get_option(0, "filetype") == "NvimTree"
+				end,
+				active = file_tree_components.active,
+				inactive = file_tree_components.active,
+			},
+		},
 	},
-    setup = {
-        theme = vim.tbl_extend("force", p, { })
-    }
+	statusline = {
+		theme = vim.tbl_extend("force", p, {}),
+        custom_providers = {
+            cwd_parent_dir = function()
+                return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+            end
+        }
+	},
 }
 
 M.config = function(lazy_plugin, opts)
 	local feline = require "feline"
-	feline.setup(opts.setup)
+	feline.setup(opts.statusline)
 	feline.winbar.setup(opts.winbar)
 end
 
